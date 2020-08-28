c8ae61f6657ddc3ec74250b93fed4db5d2706af5
Brandonza <brandon@quickconnectwireless.net>
2020-08-28 12:26:42 +0200

    Update

--- assets/js/sock.js
+++ assets/js/sock.js
@@ -1,56 +1,62 @@
-const _0x10e0 = [
-  "message",
-  "wasClean",
-  "log",
+const _0x3f03 = [
+  "onmessage",
+  "data",
   "record",
   "\x20reason=",
-  "[open]\x20Connection\x20established",
   "[message]\x20Data\x20received\x20from\x20server:\x20",
-  "[error]\x20",
+  "get",
+  "key",
+  "search",
+  "code",
+  "message",
   "onerror",
-  "[close]\x20Connection\x20died",
+  "log",
+  "send",
   "[close]\x20Connection\x20closed\x20cleanly,\x20code=",
-  "onmessage",
+  "location",
+  "wasClean",
+  "[close]\x20Connection\x20died",
+  "[error]\x20",
   "reason",
-  "client",
-  "stringify",
-  "Sending\x20to\x20server",
-  "onclose",
-  "wss://7ees1u0qxk.execute-api.af-south-1.amazonaws.com/prod/",
 ];
-(function (_0x4fdc5b, _0x10e004) {
-  const _0x39b1f0 = function (_0x232da8) {
-    while (--_0x232da8) {
-      _0x4fdc5b["push"](_0x4fdc5b["shift"]());
+(function (_0x257fa7, _0x3f03e1) {
+  const _0x451fa4 = function (_0xbe88ae) {
+    while (--_0xbe88ae) {
+      _0x257fa7["push"](_0x257fa7["shift"]());
     }
   };
-  _0x39b1f0(++_0x10e004);
-})(_0x10e0, 0x1b3);
-const _0x39b1 = function (_0x4fdc5b, _0x10e004) {
-  _0x4fdc5b = _0x4fdc5b - 0x0;
-  let _0x39b1f0 = _0x10e0[_0x4fdc5b];
-  return _0x39b1f0;
+  _0x451fa4(++_0x3f03e1);
+})(_0x3f03, 0x101);
+const _0x451f = function (_0x257fa7, _0x3f03e1) {
+  _0x257fa7 = _0x257fa7 - 0x0;
+  let _0x451fa4 = _0x3f03[_0x257fa7];
+  return _0x451fa4;
 };
-let socket = new WebSocket(_0x39b1("0xe"));
-(socket["onopen"] = function (_0x514b8f) {
-  console[_0x39b1("0x11")](_0x39b1("0x2")),
-    console["log"](_0x39b1("0xc")),
-    (data = { action: _0x39b1("0x0"), slug: _0x39b1("0xa") }),
-    socket["send"](JSON[_0x39b1("0xb")](data));
+const queryString = window[_0x451f("0x4")][_0x451f("0x10")],
+  urlParams = new URLSearchParams(queryString),
+  key = urlParams[_0x451f("0xe")](_0x451f("0xf"));
+let socket = new WebSocket(
+  "wss://7ees1u0qxk.execute-api.af-south-1.amazonaws.com/prod/"
+);
+(socket["onopen"] = function (_0x1c2519) {
+  console[_0x451f("0x1")]("[open]\x20Connection\x20established"),
+    console[_0x451f("0x1")]("Sending\x20to\x20server"),
+    (data = { action: _0x451f("0xb"), slug: key }),
+    socket[_0x451f("0x2")](JSON["stringify"](data));
 }),
-  (socket[_0x39b1("0x8")] = function (_0x57041f) {
-    console[_0x39b1("0x11")](_0x39b1("0x3") + _0x57041f["data"]);
+  (socket[_0x451f("0x9")] = function (_0xd5d7b) {
+    console[_0x451f("0x1")](_0x451f("0xd") + _0xd5d7b[_0x451f("0xa")]);
   }),
-  (socket[_0x39b1("0xd")] = function (_0x2303a7) {
-    _0x2303a7[_0x39b1("0x10")]
+  (socket["onclose"] = function (_0x3db771) {
+    _0x3db771[_0x451f("0x5")]
       ? console["log"](
-          _0x39b1("0x7") +
-            _0x2303a7["code"] +
-            _0x39b1("0x1") +
-            _0x2303a7[_0x39b1("0x9")]
+          _0x451f("0x3") +
+            _0x3db771[_0x451f("0x11")] +
+            _0x451f("0xc") +
+            _0x3db771[_0x451f("0x8")]
         )
-      : console[_0x39b1("0x11")](_0x39b1("0x6"));
+      : console[_0x451f("0x1")](_0x451f("0x6"));
   }),
-  (socket[_0x39b1("0x5")] = function (_0x4c7f02) {
-    console[_0x39b1("0x11")](_0x39b1("0x4") + _0x4c7f02[_0x39b1("0xf")]);
+  (socket[_0x451f("0x0")] = function (_0x2c79b0) {
+    console[_0x451f("0x1")](_0x451f("0x7") + _0x2c79b0[_0x451f("0x12")]);
   });
